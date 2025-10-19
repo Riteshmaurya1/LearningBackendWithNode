d019afb4c6db64d3fefcff14c91085c7886c9c11
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-10-19 11:00:12 +0530

    This is related to the module system in Node.js like common js and EJS modules.

---
+++ Modules/index.js
@@ -0,0 +1,11 @@
+//  Common js
+// let Fname = "Ritesh";
+// let Lname = "Maurya";
+
+// module.exports = { Fname,Lname };
+
+// EJS Module
+let Fname = "Ritesh";
+let Lname = "Maurya";
+
+export { Fname, Lname };
---
+++ Modules/new.js
@@ -0,0 +1,9 @@
+// Common js
+// let data = require("./index.js");
+// console.log(data);
+
+
+// EJS Module.
+import {Fname,Lname} from "./index.js";
+console.log(Fname);
+console.log(Lname);
