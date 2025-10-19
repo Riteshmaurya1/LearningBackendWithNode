6cfa05a28f904b28d4a975ce7fd2331bce24a0d9
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-10-19 14:15:00 +0530

    this is related to the os modules.

---
+++ OS file System/Osmodule.js
@@ -0,0 +1,12 @@
+const os = require("os");
+
+console.log(os.type());
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.homedir());
+console.log(os.cpus());
+console.log(os.hostname());
+console.log(os.uptime());
+console.log(os.availableParallelism());
+console.log(os.networkInterfaces());
+
