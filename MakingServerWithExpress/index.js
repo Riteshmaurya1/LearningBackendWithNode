f6f6bba7584c659fa3f237bd8585178e12545cf4
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-10-25 21:20:44 +0530

    Making server with the help of express.js

--- MakingServerWithExpress/index.js
+++ MakingServerWithExpress/index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Home Page...");
+  res
+    .header("Content-Type", "text/html")
+    .send("<h1>Hello this is Home Page</h1>");
 });
 
-app.get("/about", (req, res) => {
-  res.send("Hello from About Page...");
-});
-
-// listen the port at 3000
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log(
+    "Server is up and running on port 3000! Ready to handle requests."
+  );
 });
