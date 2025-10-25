f2c2cefc151cb381cdad033127f367635b100d84
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-10-25 15:39:22 +0530

    this is new file

---
+++ newFs/index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (req.url == "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.end(`
+            <form>
+            <label>Name:</label>
+            <input type="text" name="username"/>
+            </form>`);
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is runnnig on port no 3000");
+});
