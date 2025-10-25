c85cbd092cc5483723ed2323849bc3241004b342
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-10-25 16:31:16 +0530

    This is final commit of streams and Buffers with form data.

--- newFs/index.js
+++ newFs/index.js
@@ -1,15 +1,39 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
   if (req.url == "/") {
     res.setHeader("Content-Type", "text/html");
     res.end(`
-            <form>
+            <form action='/message' method="POST">
             <label>Name:</label>
             <input type="text" name="username"/>
-            </form>`);
+            <button type="submit">Add</button>
+            </form>
+            `);
+  } else {
+    if (req.url == "/message") {
+      res.setHeader("Content-Type", "text/html");
+      let dataChunks = [];
+      req.on("data", (chunks) => {
+        console.log(chunks);
+        dataChunks.push(chunks);
+      });
+      req.on("end", () => {
+        let combinedBuffer = Buffer.concat(dataChunks);
+        let splitedData = combinedBuffer.toString().split("=")[1];
+        fs.writeFile("BufferData.txt", splitedData, (err) => {
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          res.end();
+        });
+      });
+    } else {
+      if (req.url == "/read") {
+        fs.readFile("BufferData.txt", (err, data) => {
+          res.end(`<h1>${data.toString()}</h1>`);
+        });
+      }
+    }
   }
 });
 
