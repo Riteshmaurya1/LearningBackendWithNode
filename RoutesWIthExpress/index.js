35985fb7de8ceff39d52f8fc2ddca030e2a08df2
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-10-25 22:29:20 +0530

    this is ROutes code.

--- RoutesWIthExpress/index.js
+++ RoutesWIthExpress/index.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const app = express();
-const PORT = 3000 || 4000;
+const PORT = 3000;
 
-// const customMiddleWare = (req, res, next) => {
-//   req.user = "Guest";
-//   next();
-// };
+app.get("/products", (req, res) => {
+  res.status(200).send("Here is the list of all products.");
+});
+app.post("/products", (req, res) => {
+  res.status(201).send("A new products has been created.");
+});
+app.get("/categories", (req, res) => {
+  res.status(200).send("Here is the list of all categories.");
+});
+app.post("/categories", (req, res) => {
+  res.status(201).send("A new category has been created.");
+});
 
-app.get("/orders", (req, res) => {
-  res.status(200).send("Here is the list of all orders.");
-});
-app.post("/orders", (req, res) => {
-  res.status(201).send("A new order has been created.");
-});
-app.get("/users", (req, res) => {
-  res.status(200).send("Here is the list of all users.");
-});
-app.post("/users", (req, res) => {
-  res.status(201).send("A new user has been added.");
+app.all(/.*/, (req, res) => {
+  res.status(404).send("<h1>404 - Page Not Found</h1>");
 });
 
 app.listen(PORT, () => {
