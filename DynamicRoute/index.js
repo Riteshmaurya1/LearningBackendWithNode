e002d7c1e66d91f8429a9cc39c8b3a39dda8f9ae
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-10-26 17:58:08 +0530

    We are using the object destructuring to extract the data from params and queries.

--- DynamicRoute/index.js
+++ DynamicRoute/index.js
@@ -2,23 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/products", (req, res) => {
-  res.status(200).send("Here is the list of all products.");
-});
-app.post("/products", (req, res) => {
-  res.status(201).send("A new products has been created.");
-});
-app.get("/categories", (req, res) => {
-  res.status(200).send("Here is the list of all categories.");
-});
-app.post("/categories", (req, res) => {
-  res.status(201).send("A new category has been created.");
+app.get("/welcome/:username", (req, res) => {
+  const { username } = req.params;
+  const { role } = req.query;
+  res.send(`Welcome ${username}, your role is ${role}`);
 });
 
-app.use(/.*/, (req, res) => {
-  res.status(404).send("<h1>404 - Page Not Found</h1>");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is Running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`));
