23ca1cd260ce619dfaf2d001e6c18e6e2d084f7f
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-10-30 12:09:57 +0530

    This is Delete Commit ID.

--- SQL/Controller/studentController.js
+++ SQL/Controller/studentController.js
@@ -20,47 +20,21 @@ const Student = require("../Models/student");
 //   }
 // };
 
-const updateEntry = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { name, email } = req.body;
-
-    const student = await Student.findByPk(id);
-    if (!student) {
-      res.status(404).json({
-        message: "User Not Found",
-        status: false,
-      });
-    }
-    student.name = name;
-    student.email = email;
-    await student.save();
-  } catch (err) {
-    res.status(500).json({
-      message: err.message,
-      status: false,
-    });
-  }
-};
-
-// const deleteEntry = async (req, res) => {
+// const updateEntry = async (req, res) => {
 //   try {
 //     const { id } = req.params;
-//     const student = await Student.destroy({
-//       where: {
-//         id: id,
-//       },
-//     });
+//     const { name, email } = req.body;
+
+//     const student = await Student.findByPk(id);
 //     if (!student) {
 //       res.status(404).json({
-//         message: "User is not found.",
+//         message: "User Not Found",
 //         status: false,
 //       });
 //     }
-//     res.status(200).json({
-//       message: `User with id ${id} is deleted`,
-//       status: false,
-//     });
+//     student.name = name;
+//     student.email = email;
+//     await student.save();
 //   } catch (err) {
 //     res.status(500).json({
 //       message: err.message,
@@ -69,8 +43,34 @@ const updateEntry = async (req, res) => {
 //   }
 // };
 
+const deleteEntry = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const student = await Student.destroy({
+      where: {
+        id: id,
+      },
+    });
+    if (!student) {
+      res.status(404).json({
+        message: "User is not found.",
+        status: false,
+      });
+    }
+    res.status(200).json({
+      message: `User with id ${id} is deleted`,
+      status: false,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      status: false,
+    });
+  }
+};
+
 module.exports = {
   // addEntries,
-  updateEntry,
-  // deleteEntry,
+  // updateEntry,
+  deleteEntry,
 };
