fcee9ac4a107515bcc2609ffc0041c8fb009428e
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-10-30 12:11:15 +0530

    Final Commit ID.

--- SQL/Controller/studentController.js
+++ SQL/Controller/studentController.js
@@ -1,47 +1,47 @@
 const db = require("../utils/db-connection");
 const Student = require("../Models/student");
-// const addEntries = async (req, res) => {
-//   const { name, email } = req.body;
+const addEntries = async (req, res) => {
+  const { name, email } = req.body;
 
-//   try {
-//     const student = await Student.create({
-//       email: email,
-//       name: name,
-//     });
-//     res.status(201).json({
-//       message: `Student with name ${name} successfully added.`,
-//       success: true,
-//     });
-//   } catch (err) {
-//     res.status(500).json({
-//       message: err.message,
-//       success: false,
-//     });
-//   }
-// };
+  try {
+    const student = await Student.create({
+      email: email,
+      name: name,
+    });
+    res.status(201).json({
+      message: `Student with name ${name} successfully added.`,
+      success: true,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      success: false,
+    });
+  }
+};
 
-// const updateEntry = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { name, email } = req.body;
+const updateEntry = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
 
-//     const student = await Student.findByPk(id);
-//     if (!student) {
-//       res.status(404).json({
-//         message: "User Not Found",
-//         status: false,
-//       });
-//     }
-//     student.name = name;
-//     student.email = email;
-//     await student.save();
-//   } catch (err) {
-//     res.status(500).json({
-//       message: err.message,
-//       status: false,
-//     });
-//   }
-// };
+    const student = await Student.findByPk(id);
+    if (!student) {
+      res.status(404).json({
+        message: "User Not Found",
+        status: false,
+      });
+    }
+    student.name = name;
+    student.email = email;
+    await student.save();
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      status: false,
+    });
+  }
+};
 
 const deleteEntry = async (req, res) => {
   try {
@@ -70,7 +70,7 @@ const deleteEntry = async (req, res) => {
 };
 
 module.exports = {
-  // addEntries,
-  // updateEntry,
+  addEntries,
+  updateEntry,
   deleteEntry,
 };
--- SQL/Routes/studentRouter.js
+++ SQL/Routes/studentRouter.js
@@ -2,8 +2,8 @@ const express = require("express");
 const studentRouter = express.Router();
 const studentController = require("../Controller/studentController");
 
-// studentRouter.post("/add", studentController.addEntries);
-// studentRouter.put("/update/:id", studentController.updateEntry);
+studentRouter.post("/add", studentController.addEntries);
+studentRouter.put("/update/:id", studentController.updateEntry);
 studentRouter.delete("/delete/:id", studentController.deleteEntry);
 
 module.exports = studentRouter;
