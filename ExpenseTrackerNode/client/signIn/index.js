2e2d7a0ea39e9fb48b2de88ac21508bee981b1a2
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-11-03 19:55:02 +0530

    this is Login page of expense tracker app.

---
+++ ExpenseTrackerNode/client/signIn/index.js
@@ -0,0 +1,28 @@
+const GlobalLink = "http://localhost:3000/user";
+async function handleFormSignUp(event) {
+  event.preventDefault();
+
+  // get data from the form
+  const email = event.target.email.value.trim();
+  const password = event.target.password.value.trim();
+
+  const userData = {
+    email,
+    password,
+  };
+  console.log(userData);
+
+  //   make axios post req to the backend
+  try {
+    const response = await axios.post(`${GlobalLink}/login`, userData);
+    if (response.status === 200 || response.status === 201) {
+      alert(`Sign Up Successful! Welcome`);
+    } else {
+      alert("Sign Up Failed. Please try again.");
+    }
+  } catch (err) {
+    alert("Signup failed: " + (err.response?.data?.message || err.message));
+  }
+
+  event.target.reset();
+}
